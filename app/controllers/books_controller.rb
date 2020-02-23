dd7134191bd3c4d648659de18703eb6324770bef
yyu8k <yyu8k@icloud.com>
2020-02-23 21:25:44 +0900

    Add Create Action

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,4 +3,8 @@ class BooksController < ApplicationController
   def index
   end
 
+  def create
+    Book.create(title: book_params[:title], author: book_params[:author], text: book_params[:text], user_id: current_user.id)
+  end
+
 end
