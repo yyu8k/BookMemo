ace8aecf9d02c3daf914c86c0206192e5956bce2
yyu8k <yyu8k@icloud.com>
2020-02-23 21:27:38 +0900

    Add Strong Parameter

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,4 +7,10 @@ class BooksController < ApplicationController
     Book.create(title: book_params[:title], author: book_params[:author], text: book_params[:text], user_id: current_user.id)
   end
 
+  private
+
+  def book_params
+    params.permit(:title, :author, :text)
+  end
+
 end
