de4177c2a81de0018753bb98fe71f2a513801668
yyu8k <yyu8k@icloud.com>
2020-02-26 20:41:46 +0900

    Update books_controller.rb

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,16 +1,7 @@
 class BooksController < ApplicationController
 
   def index
-  end
-
-  def create
-    Book.create(title: book_params[:title], author: book_params[:author], text: book_params[:text], user_id: current_user.id)
-  end
-
-  private
-
-  def book_params
-    params.permit(:title, :author, :text)
+    @books = Book.all
   end
 
 end
